f9c7da3e9f73612f155c9fa1f7d98e1723b9f5f1
Luke Macken <lmacken@redhat.com>
2008-01-11 15:26:29 -0500

    Import code from our FUDCon Gobby session

--- myfedora/controllers.py
+++ myfedora/controllers.py
@@ -1,28 +1,119 @@
+import re
 import logging
+import feedparser
+import timedelta
+import datetime
 
+from testcontroller import TestController
 from packagecontroller import PackageController
 
 from turbogears import controllers, expose, flash
 from turbogears import identity, redirect
 from cherrypy import request, response
 
-from packagecontroller import PackageController
-from testcontroller import TestController
+from toscawidgets.api import Widget as TWWidget
+from myfedora.model import WidgetConfig
 
 log = logging.getLogger("myfedora.controllers")
+
+urlDataMap = {}
+LEFT, MIDDLE, RIGHT = range(3)
+
+
+class Widget(TWWidget):
+    config = {
+        'display' : None,
+        'widget' : None,
+    }
+    widgetId = None
+
+    def __init__(self, widgetId):
+        self.widgetId = widgetId
+
+        if self.widgetId:
+            self.config.display = WidgetConfig(widgetId, 'display')
+            self.config.widget = WidgetConfig(widgetId, 'widget')
+
+class RSSData(list):
+    """
+    Eventually:
+    - ability for widgets to specify filters?  so we don't have to hardcode our 
+      regex in this widget
+        - filters = ([field_to_filter, regex, results_field]) ??
+    """
+    def __init__(self, url, pollInterval):
+        self.url = url
+        self.pollInterval = pollInterval
+        self.lastChange = datetime.utcnow()
+        self.lastPoll = datetime.utcnow()
+        self.refresh(force=True)
+
+    def refresh(self, force=False):
+        if not force and (datetime.utcnow() - self.lastPoll) < self.pollInterval:
+            log.debug("Skipping refresh")
+            return
+        log.debug("Refreshing entries")
+        newEntries = []
+        regex = re.compile('<img src="(.*)" alt="" />')
+        feed = feedparser.parse(self.url)
+        for entry in feed['entries'][:5]: # FIXME: use config info
+            newEntries.append({
+                'link'  : entry['link'],
+                'title' : entry['title'],
+                'image' : regex.match(entry['summary']).group(1)
+            })
+        for entry in range(0, newEntries):
+            if newEntries[entry].link != self[entry].link:
+                self[:]
+                self.extend(newEntries)
+                self.lastChange = datetime.utcnow()
+                break
+        self.lastPoll = datetime.utcnow()
+
+class RSSWidget(Widget):
+    template = 'fedora.widgets.rsswidget'
+    pollInterval = timedelta(minutes=15)
+
+    def __init__(self, widgetId, title, url):
+        super(RSSWidget).__init__(self, widgetId, url=None)
+        self.url = url
+        self.title = title
+        try:
+            self.entries = urlDataMap[url]
+        except KeyError:
+            urlDataMap[url] = RSSData(url, self.pollInterval)
+        self.entries.refresh()
+
+    def update_params():
+        print "update_params()"
+
+    def __json__(self):
+        return {'widgetId': self.widgetId, 'url':self.url, 'entries': self.entries}
+
+
+class FedoraPeopleWidget(RSSWidget):
+    url = 'http://planet.fedoraproject.org/rss20.xml'
+
 
 class Root(controllers.RootController):
     # /packages/ is used for the package views
     packages = PackageController()
     test = TestController()
 
-    @expose(template="myfedora.templates.welcome")
-    # @identity.require(identity.in_group("admin"))
+    @expose(template='myfedora.templates.index', allow_json=True)
     def index(self):
-        import time
-        # log.debug("Happy TurboGears Controller Responding For Duty")
-        flash("Your application is now running")
-        return dict(now=time.ctime())
+        widgets = ([], [], [])
+        if identity.anonymous():
+            # use defaults
+            widgets[LEFT].append(FedoraPeopleWidget())
+        else:
+            ### FIXME:
+            # figure out what widgets user wants to display
+            # instantiate widgets with custom configuration
+            pass
+        return {
+            'widgets': widgets
+        }
 
     @expose(template="myfedora.templates.login")
     def login(self, forward_url=None, previous_url=None, *args, **kw):
