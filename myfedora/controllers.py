975c82a813e6624958420df1ead20f4aee12e3f5
Luke Macken <lmacken@redhat.com>
2008-01-11 16:14:41 -0500

    Fix another template widget typo

--- myfedora/controllers.py
+++ myfedora/controllers.py
@@ -70,7 +70,7 @@ class RSSData(list):
         self.lastPoll = datetime.utcnow()
 
 class RSSWidget(Widget):
-    template = 'myfedora.widgets.rsswidget'
+    template = 'myfedora.templates.rsswidget'
     pollInterval = timedelta(minutes=15)
 
     def __init__(self, widgetId, title = None, url = None):
