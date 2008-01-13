9441cb160b70c1ba25f9c40fdf6ba5323ab7f4a3
Toshio Kuratomi <badger@Clingman.(none)>
2008-01-13 11:05:04 -0500

    * Commit code to instantiate the javascript widget.

--- myfedora/controllers.py
+++ myfedora/controllers.py
@@ -6,11 +6,13 @@ from testcontroller import TestController
 from packagecontroller import PackageController
 
 from datetime import timedelta, datetime
-from turbogears import controllers, expose, flash
+from turbogears import controllers, expose, flash, url
 from turbogears import identity, redirect
 from cherrypy import request, response
 
 from toscawidgets.api import Widget as TWWidget
+from toscawidgets.api import JSLink as TWJSLink
+
 from myfedora.model import WidgetConfig
 
 log = logging.getLogger("myfedora.controllers")
@@ -41,8 +43,8 @@ class RSSData(list):
       regex in this widget
         - filters = ([field_to_filter, regex, results_field]) ??
     """
-    def __init__(self, url, pollInterval):
-        self.url = url
+    def __init__(self, feedUrl, pollInterval):
+        self.url = feedUrl
         self.pollInterval = pollInterval
         self.lastChange = datetime.utcnow()
         self.lastPoll = datetime.utcnow()
@@ -56,7 +58,7 @@ class RSSData(list):
         newEntries = []
         regex = re.compile('<img src="(.*)" alt="" />')
         feed = feedparser.parse(self.url)
-        for entry in feed['entries'][:5]: # FIXME: use config info
+        for entry in feed['entries']:
             newEntries.append({
                 'link'  : entry['link'],
                 'title' : entry['title']
@@ -78,17 +80,24 @@ class RSSData(list):
             
         self.lastPoll = datetime.utcnow()
 
+class LocalTWJSLink(TWJSLink):
+    def update_params(self, d):
+        super(TWJSLink, self).update_params(d)
+        d["link"] = url(self.filename)
+
 class RSSWidget(Widget):
+    javascript = [ LocalTWJSLink(modname='myfedora',
+        filename='/static/js/fedorawidgets.js')]
+
     template = 'myfedora.templates.rsswidget'
     pollInterval = timedelta(minutes=15)
-    params = ["title", "entries", "maxEntries"]
-    entries = 5
+    params = ["widgetId", "url", "title", "entries", "maxEntries"]
 
     def __init__(self, widgetId, title = None, url = None, maxEntries = None):
         super(RSSWidget, self).__init__(widgetId)
         self.url = url or self.url
         self.title = title or self.title
-        self.entries = maxEntries or self.entries
+        self.maxEntries = maxEntries or 5
         try:
             self.entries = urlDataMap[self.url]
         except KeyError:
@@ -96,16 +105,14 @@ class RSSWidget(Widget):
             self.entries = urlDataMap[self.url]
         self.entries.refresh()
 
-
     def __json__(self):
         return {'widgetId': self.widgetId, 'url':self.url, 'entries': self.entries}
-
 
 class FedoraPeopleWidget(RSSWidget):
     url = 'http://planet.fedoraproject.org/rss20.xml'
     title = 'Fedora People'
 
-class RawhideBugWidget(RSSWidget):
+class RawhideBugsWidget(RSSWidget):
     url = 'https://bugzilla.redhat.com/buglist.cgi?bug_file_loc_type=allwordssubstr&bug_status=ASSIGNED&bug_status=MODIFIED&bug_status=NEEDINFO&bug_status=NEW&bugidtype=include&changedin=7&chfield=%5BBug%20creation%5D&chfieldto=Now&cust_facing_type=substring&devel_whiteboard_type=allwordssubstr&emailassigned_to1=1&emailassigned_to2=1&emailcc2=1&emailqa_contact2=1&emailreporter2=1&emailtype1=exact&emailtype2=exact&field0-0-0=noop&fixed_in_type=allwordssubstr&keywords_type=allwords&long_desc_type=substring&product=Fedora&qa_whiteboard_type=allwordssubstr&query_format=advanced&short_desc_type=allwordssubstr&status_whiteboard_type=allwordssubstr&type0-0-0=noop&version=rawhide&ctype=rss'
     title = 'Rawhide Bugs'
 
@@ -114,26 +121,68 @@ class FedoraUpdatesWidget(RSSWidget):
     title = 'Fedora Updates'
 
 
+class WidgetsController(controllers.Controller):
+    '''Controller that serves an instance of a widget.
+
+    This can be used to either:
+    1. Serve a widget to another process (possibly over Comet)
+    2. Serve data about a widget (once again, possibly over JSON)
+    '''
+
+    @expose(template='myfedora.templates.widget', allow_json=True)
+    def RSS(self, widgetId, title = None, url = None, maxEntries = None, **kwargs):
+        '''Serve the RSS Widget.'''
+        print kwargs
+        print RSSWidget
+        print (dir (RSSWidget))
+        rss = RSSWidget(widgetId, title, url, maxEntries)
+        return { widget:rss }
+
+    @expose(template='myfedora.templates.widget', allow_json=True)
+    def FedoraPeople(self, widgetId):
+        '''Serve the FedoraPeopleWidget.'''
+        fedorapeople = FedoraPeopleWidget(widgetId)
+        return { widget:fedorapeople }
+
+    @expose(template='myfedora.templates.widget', allow_json=True)
+    def RawhideBugs(self, widgetId):
+        '''Serve the FedoraPeopleWidget.'''
+        rawhidebugs = RawhideBugs(widgetId)
+        return { widget:rawhidebugs }
+
+    @expose(template='myfedora.templates.widget', allow_json=True)
+    def FedoraUpdates(self, widgetId):
+        '''Serve the FedoraPeopleWidget.'''
+        fedoraupdates = FedoraUpdatesWidget(widgetId)
+        return { widget:fedoraupdates }
+
 class Root(controllers.RootController):
     # /packages/ is used for the package views
     packages = PackageController()
     test = TestController()
+    widgets = WidgetsController()
 
     @expose(template='myfedora.templates.index', allow_json=True)
     def index(self):
+        ### FIXME:
+        # This doesn't quite work because we don't recurse into the container
+        # to find the javascript entries for the widgets.  Probably need a
+        # new container class that extracts javascript and css properties from
+        # the widgets.
         widgets = ([], [], [])
         if identity.current.anonymous:
             # use defaults
             widgets[LEFT].append(FedoraPeopleWidget('people1'))
             widgets[RIGHT].append(FedoraUpdatesWidget('updates1'))
-            widgets[RIGHT].append(RawhideBugWidget('rawhide1'))
+            widgets[RIGHT].append(RawhideBugsWidget('rawhide1'))
         else:
             ### FIXME:
             # figure out what widgets user wants to display
             # instantiate widgets with custom configuration
             pass
+
         return {
-            'widgets': widgets
+            'widgets': widgets,
         }
 
     @expose(template="myfedora.templates.login")
