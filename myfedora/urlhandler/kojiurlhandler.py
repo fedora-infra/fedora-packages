8d06d2e3c9b5d11f200f17d953c4cd5405b83b1a
John (J5) Palmieri <johnp@redhat.com>
2008-01-25 15:16:40 -0500

    much better search using koji

--- myfedora/urlhandler/kojiurlhandler.py
+++ myfedora/urlhandler/kojiurlhandler.py
@@ -10,9 +10,13 @@ class KojiURLHandler(URLHandler):
 
         self.package_path = 'koji/packageinfo?packageID='
         self.xml_rpc_path = 'kojihub'
+        self.xml_rpc_url = self.get_base_url() + self.xml_rpc_path
 
     def get_package_url(self, pkg_name):
-        cs = koji.ClientSession(self.get_base_url() + self.xml_rpc_path)
+        cs = koji.ClientSession(self.xml_rpc_url)
         pkg = cs.getPackage(pkg_name)
 
         return self.get_base_url() + self.package_path + str(pkg['id'])
+
+    def get_xml_rpc_url(self):
+        return self.xml_rpc_url
