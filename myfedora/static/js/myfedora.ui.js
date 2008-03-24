64123aa38bfae941bfd98c861c46a8bdd83e512e
John (J5) Palmieri <johnp@redhat.com>
2008-03-24 13:55:03 -0400

    use params instead of writing them into the uri

--- myfedora/static/js/myfedora.ui.js
+++ myfedora/static/js/myfedora.ui.js
@@ -25,16 +25,25 @@ function mf_menu_hover_stop()
     _mf_current_hover_menu = "";
 }
 
-function mf_replace_block_async(block_name, url, user_callback, user_data)
+function mf_replace_block_async(block_name, url, params, user_callback, user_data)
 {
-    $.getJSON(url + "&tg_format=json", 
-      function (data) 
+    function _mf_json_callback(data) 
           {
             var block = $("#" + block_name);
             var html = user_callback(block, data, user_data);
             if (html != undefined)
               block.html(html);
           } 
-    ); 
+
+    if (params == undefined)
+      params = {};
+
+    if (params.callback == '?') {
+      delete(params.callback);
+      url += '?callback=?';
+    }
     
+    params.tg_format = 'json';
+
+    $.getJSON(url, params, _mf_json_callback); 
 }
