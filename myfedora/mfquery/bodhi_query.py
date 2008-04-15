aeffeb6df1d90b2888e4dd8ee6a6808e000e27bc
John (J5) Palmieri <johnp@redhat.com>
2008-04-15 15:54:00 -0400

    split controler and helper function

--- myfedora/mfquery/bodhi_query.py
+++ myfedora/mfquery/bodhi_query.py
@@ -4,28 +4,33 @@ from turbogears import controllers, expose, identity, config
 
 from myfedora.urlhandler import BodhiURLHandler
 
+def get_info(*args, **kw):
+   kw.update({
+       'tg_format': 'json'
+   })
+
+    print kw['tg_format']
+    if not kw.get('package', None):
+        return {}
+
+    url = BodhiURLHandler().get_base_url() + 'list?' 
+    url += urllib.urlencode(kw)
+
+    cj = cookielib.CookieJar()
+
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    request = urllib2.Request(url)
+    cookieName = config.get('visit.cookie.name', 'tg-visit')
+    request.add_header("Cookie", cookieName + "=" + identity.current.visit_key)
+    response = opener.open(request)
+
+    json_data = simplejson.load(response)
+        
+    return json_data
+
+
 class BodhiQuery(controllers.Controller):
     @expose("json", allow_json=True)
-    def get_info(self, *args, **kw):
-        kw.update({
-            'tg_format': 'json'
-        })
-
-        if not kw.get('package', None):
-            return {}
-
-        url = BodhiURLHandler().get_base_url() + 'list?' 
-        url += urllib.urlencode(kw)
-
-        cj = cookielib.CookieJar()
-
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-        request = urllib2.Request(url)
-        cookieName = config.get('visit.cookie.name', 'tg-visit')
-        request.add_header("Cookie", cookieName + "=" + identity.current.visit_key)
-        response = opener.open(request)
-
-        json_data = simplejson.load(response)
-        
-        return json_data
+    def get_info(self, *args, **kw): 
+        return get_info(*args, **kw)
 
