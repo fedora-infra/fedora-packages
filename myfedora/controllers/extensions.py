27ec9ed03663e715b7c0422359e98616637fde6d
John (J5) Palmieri <johnp@redhat.com>
2008-07-02 17:59:53 -0400

    wrote loader for the javascript extensions
    Files must reside in the myfedora/plugins/extensions folder though there is a
    FIXME item which notes that we should be using the pkg_resources facilities
    for finding the directory (so that it can reside in /usr/share/bla bla bla or
    similar)

--- myfedora/controllers/extensions.py
+++ myfedora/controllers/extensions.py
@@ -1,6 +1,24 @@
 from myfedora.lib.base import Controller
 from tg import expose
 import pylons
+import shlex
+import os
+
+class Chunk(object):
+    def __init__(self):
+        self.consumes = []
+        self.info = ""
+        self.code = ""
+        
+    def parse_consumes_field(self):
+        start = self.info.find('consumes') + 8
+        start = self.info.find('[',start)
+        end = self.info.find(']', start)
+        
+        consumes = self.info[start + 1 : end]
+        print consumes
+        self.consumes = shlex.split(consumes)
+        print consumes  
 
 class ExtensionsController(Controller):
     """
@@ -21,7 +39,7 @@ class ExtensionsController(Controller):
     
     Extensions also need a header which describes the extension as such:
     
-    info = {'consumes':['build_message'],
+    info : {'consumes':['build_message'],
             'author': 'John (J5) Palmieri <johnp@redhat.com>',
             'version': '0.1',
             'name': 'Hello World Message'
@@ -32,49 +50,96 @@ class ExtensionsController(Controller):
         self.__debug = debug # if debug is False condense javascript to optimize
         self.__extension_cache = {}
         
-        #lets put a dummy in for now to test
-        self.__extension_cache['build_message'] = """
-        [{ 
-           info: {consumes:['build_message'],
-                 author: 'John (J5) Palmieri <johnp@redhat.com>',
-                 version: '0.1',
-                 name: 'Hello World Message'
-           },
-         
-           run: function (data) {
-            
-             msg = this.HelloWorld(data.task_id);
-            
-             jQuery("#" + data.uid).css('background-color', 'blue');
-             return(msg); 
-           },
-         
-           HelloWorld: function (name) {
-             return("Hello " + name);
-           }
-         },
-         { 
-           info: {consumes:['build_message'],
-                 author: 'John (J5) Palmieri <johnp@redhat.com>',
-                 version: '0.1',
-                 name: 'Goodbye World Message'
-           },
-         
-           run: function (data) {
-            
-             msg = this.GoodbyeWorld(data.task_id);
-            
-             jQuery("#" + data.uid).css('background-color', 'red');
-             return(msg); 
-           },
-         
-           GoodbyeWorld: function (name) {
-             return("Goodbye " + name);
-           }
-         }
-        ]
-        """
+        # FIXME: use pkg_resources to find the extensions directories 
+        self.load_extensions('myfedora/plugins/extensions')
     
+    def chunk_code(self, js, filename):
+        start = js.find('{')
+        end = 0
+        chunks = []
+        length = len(js)
+        
+        while (start != -1):
+            count = 1
+            i = start + 1
+            pull_info = False
+            pull_info_start = 0
+            pull_info_end = 0
+            
+            while(count != 0 and i < length):
+                if js[i] == '{':
+                    count += 1
+                elif js[i] == '}':
+                    count -= 1
+                elif count==1 and js[i]=='i' and js.find('info',i) == i:
+                    if pull_info_start != 0:
+                        print "ERROR: Unexpected Info Field : extension file %s has more than on info field per extension (ignoring file)" % filename
+                        return None
+                    
+                    # put us on info duty
+                    pull_info = True
+                    
+                if pull_info:
+                    if pull_info_start == 0:
+                        if count > 1:
+                            pull_info_start = i
+                    else:
+                        if count == 1:
+                            pull_info_end = i
+                            pull_info = False
+                    
+                i += 1
+            
+            end = i
+            
+            c = Chunk()
+            c.code = js[start:end]
+            c.info = js[pull_info_start:pull_info_end + 1]
+            print c.code
+            print c.info
+            
+            c.parse_consumes_field()
+            chunks.append(c)
+            
+            if i >= length:
+                print "ERROR: Unexpected EOF: extension file %s has mismatched braces (ignoring file)" % filename
+                return None
+                
+            start = js.find('{', end)
+        
+        return chunks
+    
+    def load_extension(self, file):
+        f = open(file,'r')
+        js = f.read()
+        
+        chunks = self.chunk_code(js, file)
+        if not chunks:
+            if chunks != None:
+                print "ERROR: No Content : extension file %s doesn't have any valid extensions (ignoring file)" % filename
+                
+            return
+        
+        for c in chunks:
+            for exttype in c.consumes:
+                code = self.__extension_cache.get(exttype, [])
+                #TODO: run through optimizer which strips whitespace
+                code.append(c.code)
+                self.__extension_cache[exttype] = code
+        
+    def load_extensions(self, dir):
+        for root, dirs, files in os.walk(dir):
+            for name in files:
+                if name.endswith('js'):
+                    path = os.path.join(root, name)
+                    self.load_extension(path)
+                    
+        # compile lists in the cache down to a string so we don't have to 
+        # process it on each request
+        for key, value in self.__extension_cache.iteritems():
+            s = '[' + ','.join(value) + ']'
+            self.__extension_cache[key] = s
+                
     @expose()
     def default(self, exttype):
         return self.__extension_cache.get(exttype, "");
