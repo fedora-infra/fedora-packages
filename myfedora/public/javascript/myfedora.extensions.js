e95ee7cacca504bf116787dde0d4061d637e7a83
John (J5) Palmieri <johnp@redhat.com>
2008-07-02 16:18:39 -0400

    make cache work efficently
    * myfedora/public/javascript/myfedora.extensions.js
      (extensions.load_extensions): defer all requests for the same extension type
      until the first request returns which cuts down on the number of network
      roundtrips we need to do.  Also, eval the javascript in load_extentions and
      cache that instead of the string so that we only have to eval an extension
      once.

--- myfedora/public/javascript/myfedora.extensions.js
+++ myfedora/public/javascript/myfedora.extensions.js
@@ -2,11 +2,12 @@ var _extensions = function(){};
 
 _extensions.prototype = {
     _extension_cache: {},
+    _extension_deferred: {},
     _extension_uid_counter: 0,
         
     grep_extensions: function()
       {
-        var extension_points = $('script[type=text/x-myfedora-extpoint]');
+        var extension_points = jQuery('script[type=text/x-myfedora-extpoint]');
         extension_points.each
           (
             function(i)
@@ -21,13 +22,8 @@ _extensions.prototype = {
     
     run_extensions: function (js, data)
       {
+        var obj_list = js;
         
-        //we trust the server so we can just eval the js
-        // it should be a list of objects with a run method
-        var obj_list = eval(js);
-        
-        console.log(obj_list);
-        console.log(obj_list.length);
         for(var i = 0; i < obj_list.length; i++) {
           //clone our data and generate our uid so we are unique
           var d = new myfedora.shallow_clone(data);
@@ -36,14 +32,20 @@ _extensions.prototype = {
             
           //create a place to put this extension that they can call their own
           var div = jQuery("<div />").attr("id", d.uid);
-          
+          div.hide();
           var append_div = jQuery("#" + data.placeholder_id);
           append_div.append(div);
           
           //run the script and parse in the results
-          console.log(i);
           var result = obj_list[i].run(d);
           div.append(result);
+          
+          //show it
+          if (data.effect) {
+            eval("div." + data.effect);
+          } else {
+            div.show();
+          }
           
           this._extension_uid_counter++;
         }
@@ -53,17 +55,39 @@ _extensions.prototype = {
     load_extensions: function (data) 
       {
         var ext_code = this._extension_cache[data.type];
+        var ext_deferred = this._extension_deferred[data.type];
         if (ext_code)
           {
-            this.run_extensions(ext_code, data)
+            this.run_extensions(ext_code, data);
+            return; 
+          }
+        else if (ext_deferred)
+          { 
+            this._extension_deferred[data.type].push(data);
             return;
           }
         
+        this._extension_deferred[data.type] = new Array();
         jQuery.get("/extensions?exttype=" + data.type, 
-          function (js, err)
+          function (js_string, err)
             {
-              myfedora.extensions._extension_cache[data.type] = js;
+              // we trust the server so we can just eval the js
+              // it should be a list of objects with a run method
+              var js = eval(js_string);
+              
+              // run this callback's extention
               myfedora.extensions.run_extensions(js, data);
+              
+              // now run the deferred extensions queued up while the scripts
+              // were being downloaded
+              var d = myfedora.extensions._extension_deferred[data.type].shift();
+              while(d)
+                {
+                  myfedora.extensions.run_extensions(js, d);
+                  d = myfedora.extensions._extension_deferred[data.type].shift();
+                }
+                
+              myfedora.extensions._extension_cache[data.type] = js;
             } 
          );
       }
