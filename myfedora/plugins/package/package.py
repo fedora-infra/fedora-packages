a6b286fca028715a95b00232558318ba1669baec
John (J5) Palmieri <johnp@redhat.com>
2008-07-30 15:54:34 -0400

    remove dead files

--- myfedora/plugins/package/package.py
+++
@@ -1,8 +0,0 @@
-from tg import expose
-from myfedora.lib.base import BaseController
-
-class PackageController(BaseController):
-
-    @expose('myfedora.plugins.package.templates.package')
-    def index(self, *args, **kwargs):
-        print "PackageController.index!!!"
