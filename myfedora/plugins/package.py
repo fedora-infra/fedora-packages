b8c4aca1d2167c1842fad26252929e4e75150022
John (J5) Palmieri <johnp@redhat.com>
2008-08-04 14:21:09 -0400

    remove leftover from old versions

--- myfedora/plugins/package.py
+++
@@ -1,9 +0,0 @@
-from tg import expose
-from myfedora.lib.base import BaseController
-
-class PackageController(BaseController):
-
-    @expose('genshi:package')
-    def index(self, *args, **kwargs):
-        print "PackageController.index!!!"
-        return dict(data="Booyah")
