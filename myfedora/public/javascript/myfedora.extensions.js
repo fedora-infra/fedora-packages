43ab4a9b4685fbfb81ada33fa950ec52dfac7e73
John (J5) Palmieri <johnp@redhat.com>
2008-11-06 12:23:44 -0500

    add try catch blocks to the javascript

--- myfedora/public/javascript/myfedora.extensions.js
+++ myfedora/public/javascript/myfedora.extensions.js
@@ -14,24 +14,36 @@ _extensions.prototype = {
       },
     grep_extensions: function()
       {
-        var extension_points = jQuery('script[type=text/x-myfedora-extpoint]');
-        extension_points.each
-          (
-            function(i)
-              {
-                var ep = extension_points[i]
-                var data = ep.text;
-                data = myfedora.safe_json_parse(data);
-                myfedora.extensions.load_extensions(data);
-              }                 
-          );
+        try 
+          {
+            var extension_points = jQuery('script[type=text/x-myfedora-extpoint]');
+            extension_points.each
+              (
+                function(i)
+                  {
+                    var ep = extension_points[i]
+                    var data = ep.text;
+                    data = myfedora.safe_json_parse(data);
+                    myfedora.extensions.load_extensions(data);
+                  }                 
+              );
+          }
+        catch(err)
+          {
+          }
       },
     load_ui: function(ui, js)
       {
-        id = js.placeholder_id;
-        ui = eval("new myfedora.ui." + ui + "('" + id + "')");
-        ui.set_show_effect(js.show_effect);
-        ui.set_hide_effect(js.hide_effect);
+        try
+          {
+            id = js.placeholder_id;
+            ui = eval("new myfedora.ui." + ui + "('" + id + "')");
+            ui.set_show_effect(js.show_effect);
+            ui.set_hide_effect(js.hide_effect);
+          }
+        catch(err)
+          {
+          }
       },
     run_extensions: function (js, data)
       {
@@ -40,53 +52,57 @@ _extensions.prototype = {
             return;
           }
       
-        var obj_list = js;
-        
-        if (data.hide_parent)
-          jQuery("#" + data.hide_parent).hide();
-        
-        var append_div = jQuery("#" + data.placeholder_id);
-        
-        var block_element;
-        if (append_div.is('ul') ||
-            append_div.is('ol'))
+        try 
           {
-            block_element = jQuery('<li/>')
+            var obj_list = js;
+        
+            if (data.hide_parent)
+              jQuery("#" + data.hide_parent).hide();
+            
+            var append_div = jQuery("#" + data.placeholder_id);
+            
+            var block_element;
+            if (append_div.is('ul') ||
+                append_div.is('ol'))
+              {
+                block_element = jQuery('<li/>')
+              }
+            else
+              {
+                block_element = jQuery('<div/>')
+              }
+              
+            for(var i = 0; i < obj_list.length; i++) {
+              //clone our data and generate our uid so we are unique
+              var d = new myfedora.shallow_clone(data);
+              d.uid = "extension_" + this.get_uid();
+                
+              //create a place to put this extension that they can call their own
+              var div = block_element.attr("id", d.uid);
+              
+              append_div.append(div);
+              
+              //run the script and parse in the results
+              var result = obj_list[i].run(d);
+              if (!result)
+                {
+                  return;
+                }
+                
+              div.append(result);
+              
+              if (!data.ui) {
+                if (data.show_effect) {
+                  eval("append_div." + data.show_effect);
+                } else {
+                  div.show();
+                }
+              }
+            }
           }
-        else
+        catch(err)
           {
-            block_element = jQuery('<div/>')
           }
-          
-        for(var i = 0; i < obj_list.length; i++) {
-          //clone our data and generate our uid so we are unique
-          var d = new myfedora.shallow_clone(data);
-          d.uid = "extension_" + this.get_uid();
-            
-          //create a place to put this extension that they can call their own
-          var div = block_element.attr("id", d.uid);
-          
-          append_div.append(div);
-          
-          //run the script and parse in the results
-          var result = obj_list[i].run(d);
-          if (!result)
-            {
-              return;
-            }
-            
-          div.append(result);
-          
-          if (!data.ui) {
-            if (data.show_effect) {
-              eval("append_div." + data.show_effect);
-            } else {
-              div.show();
-            }
-          }
-          
-        }
-       
       },
     
     load_extensions: function (data) 
