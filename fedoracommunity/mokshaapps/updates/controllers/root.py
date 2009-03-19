ddcc7f114fe9aa988c8b8f08f1c12e71d671c931
John (J5) Palmieri <johnp@redhat.com>
2009-03-19 14:53:39 -0400

    add the updates overview front page and filtering

--- fedoracommunity/mokshaapps/updates/controllers/root.py
+++ fedoracommunity/mokshaapps/updates/controllers/root.py
@@ -2,19 +2,104 @@ from tg import expose, tmpl_context, validate
 from formencode import validators
 
 from moksha.lib.base import Controller
-from moksha.api.widgets import ContextAwareWidget, Grid
+
+from moksha.lib.helpers import Category, MokshaApp, Not, not_anonymous, MokshaWidget, Widget, check_predicates
+from moksha.api.widgets import ContextAwareWidget, Grid, Selectable
+from moksha.api.widgets.containers import DashboardContainer
+
+from tw.api import JSLink
+from tw.jquery import jQuery, jquery_js, js_callback
+
 
 class UpdatesGrid(Grid, ContextAwareWidget):
     template='mako:fedoracommunity.mokshaapps.updates.templates.table_widget'
 
+class UpdatesFilter(Selectable):
+    updates_filter_js = JSLink(modname='fedoracommunity.mokshaapps.updates',
+                              filename='javascript/updatesfilter.js')
+    javascript = [updates_filter_js] + Selectable.javascript
+
+    def update_params(self, d):
+        super(UpdatesFilter, self).update_params(d)
+        categories = []
+
+        if check_predicates(not_anonymous()):
+            cat = {
+                  }
+
+            categories.append(cat)
+
+
+        cat = {'label': 'All Packages',
+               'items': [{'label': 'Unpushed Updates',
+                          'link':'javascript:void(0);',
+                          'data':{
+                                  'rows_per_page': 10,
+                                  'filters': {'status':'pending'}
+                                 }
+                         },
+                         {'label': 'Testng Updates',
+                          'link':'javascript:void(0);',
+                          'data': {
+                                   'rows_per_page': 10,
+                                   'filters':{'status':'testing'}
+                                  }
+                         },
+                         {'label': 'Stable Updates',
+                          'link':'javascript:void(0);',
+                          'data': {
+                                   'rows_per_page': 10,
+                                   'filters':{'status':'stable'}
+                                  }
+                         }]
+                }
+
+        categories.append(cat)
+        d.update({'categories': categories})
+
+        self.add_call('$("#%s").bind("selected", _updates_filter_selected)' % d.content_id)
+
+updates_filter = UpdatesFilter('updates_filter')
 updates_grid = UpdatesGrid('updates_table')
+
+class UpdatesContainer(DashboardContainer, ContextAwareWidget):
+    unpushed_updates_app = MokshaApp('Unpushed Updates', 'fedoracommunity.updates/table',
+                                       css_class='main_table',
+                                       params={'rows_per_page': 5,
+                                               'filters':{'status':'pending'}})
+    testing_updates_app = MokshaApp('Testing Updates', 'fedoracommunity.updates/table',
+                                       css_class='secondary_table',
+                                       params={'rows_per_page': 5,
+                                               'filters':{'status':'testing'}})
+    stable_updates_app = MokshaApp('Stable Updates', 'fedoracommunity.updates/table',
+                                       css_class='secondary_table',
+                                       params={'rows_per_page': 5,
+                                               'filters':{'status':'stable'}})
+
+    layout = [Category('right-content-column',
+                        (Widget('Filters', updates_filter
+                                 ),
+                        MokshaApp('Alerts', 'fedoracommunity.alerts'))),
+              Category('left-content-column',
+                        (unpushed_updates_app,
+                        testing_updates_app,
+                        stable_updates_app))]
+
+    def update_params(self, d):
+        super(UpdatesContainer, self).update_params(d)
+
+updates_container = UpdatesContainer('updates_container')
 
 class RootController(Controller):
 
     # do something for index, this should be the container stuff
-    @expose()
-    def index(self):
-        return {}
+    @expose('mako:moksha.templates.widget')
+    def index(self, **kwds):
+        options = {}
+
+        tmpl_context.widget = updates_container
+        return {'options':options}
+
 
     @expose('mako:fedoracommunity.mokshaapps.updates.templates.table')
     @validate(validators={'rows_per_page': validators.Int()})
---
+++ fedoracommunity/mokshaapps/updates/javascript/updatesfilter.js
@@ -0,0 +1,14 @@
+function _updates_filter_selected(o, d) {
+  data = $(d.element).data('.moksha_selectable_data');
+  console.log(data)
+
+  if (data.show_secondary_tables) {
+    $('.secondary_table').fadeIn();
+  } else {
+    $('.secondary_table').fadeOut();
+  }
+
+  var main_table = $('.main_table');
+  $('h3', main_table).html(data.label);
+  $('table', main_table).mokshagrid("request_update", data);
+}
