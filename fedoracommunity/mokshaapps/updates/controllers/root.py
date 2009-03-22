6e97363086680a5dcf81538782cced7f9503400f
Luke Macken <lmacken@redhat.com>
2009-03-21 23:37:38 -0400

    Create another Updates Container for authenticated users.

--- fedoracommunity/mokshaapps/updates/controllers/root.py
+++ fedoracommunity/mokshaapps/updates/controllers/root.py
@@ -31,80 +31,151 @@ class UpdatesFilter(Selectable):
         categories = []
 
         if check_predicates(not_anonymous()):
-            cat = {
-                  }
+            categories.append({
+                'label': 'Packages I Own',
+                'items': [
+                    {
+                        'label': 'Unpushed Updates',
+                        'link':'javascript:void(0);',
+                        'data': {
+                            'rows_per_page': 10,
+                            'filters': {
+                                'status': 'pending',
+                                'profile': True,
+                            },
+                        }
+                    },
+                    {
+                        'label': 'Testing Updates',
+                        'link':'javascript:void(0);',
+                        'data': {
+                            'rows_per_page': 10,
+                            'filters': {
+                                'status': 'testing',
+                                'profile': True,
+                            },
+                        }
+                    },
+                    {
+                        'label': 'Stable Updates',
+                        'link':'javascript:void(0);',
+                        'data': {
+                            'rows_per_page': 10,
+                            'filters': {
+                                'status':'stable',
+                                'profile': True,
+                            },
+                        }
+                    }
+                ]
+            })
 
-            categories.append(cat)
+        categories.append({
+            'label': 'All Packages',
+            'items': [
+                {
+                    'label': 'Unpushed Updates',
+                    'link':'javascript:void(0);',
+                    'data': {
+                        'rows_per_page': 10,
+                        'filters': {'status': 'pending'},
+                     }
+                },
+                {
+                    'label': 'Testing Updates',
+                    'link':'javascript:void(0);',
+                    'data': {
+                        'rows_per_page': 10,
+                        'filters':{'status': 'testing'},
+                    }
+                },
+                {
+                    'label': 'Stable Updates',
+                    'link':'javascript:void(0);',
+                    'data': {
+                        'rows_per_page': 10,
+                        'filters':{'status': 'stable'},
+                    }
+                 }
+            ]
+        })
 
-
-        cat = {'label': 'All Packages',
-               'items': [{'label': 'Unpushed Updates',
-                          'link':'javascript:void(0);',
-                          'data':{
-                                  'rows_per_page': 10,
-                                  'filters': {'status':'pending'}
-                                 }
-                         },
-                         {'label': 'Testing Updates',
-                          'link':'javascript:void(0);',
-                          'data': {
-                                   'rows_per_page': 10,
-                                   'filters':{'status':'testing'}
-                                  }
-                         },
-                         {'label': 'Stable Updates',
-                          'link':'javascript:void(0);',
-                          'data': {
-                                   'rows_per_page': 10,
-                                   'filters':{'status':'stable'}
-                                  }
-                         }]
-                }
-
-        categories.append(cat)
         d.update({'categories': categories})
 
-        self.add_call('$("#%s").bind("selected", _updates_filter_selected)' % d.content_id)
+        self.add_call('$("#%s").bind("selected", _updates_filter_selected)' %
+                      d.content_id)
 
 updates_filter = UpdatesFilter('updates_filter')
-updates_grid = UpdatesGrid('updates_table')
+#updates_grid = UpdatesGrid('updates_table')
+
+class AnonymousUpdatesContainer(DashboardContainer, ContextAwareWidget):
+    layout = [
+            Category('right-content-column', (
+                Widget('Filters', updates_filter),
+                MokshaApp('Alerts', 'fedoracommunity.alerts')
+                )),
+            Category('left-content-column', (
+                MokshaApp('Unpushed Updates', 'fedoracommunity.updates/table',
+                          css_class='main_table', params={'rows_per_page': 5,
+                              'filters': {'status':'pending'}}),
+                MokshaApp('Testing Updates', 'fedoracommunity.updates/table',
+                          css_class='secondary_table', params={
+                              'rows_per_page': 5,
+                              'filters': {'status':'testing'}}),
+                MokshaApp('Stable Updates', 'fedoracommunity.updates/table',
+                          css_class='secondary_table', params={
+                              'rows_per_page': 5,
+                              'filters': {'status':'stable'}}),
+                ))
+    ]
 
 class UpdatesContainer(DashboardContainer, ContextAwareWidget):
-    unpushed_updates_app = MokshaApp('Unpushed Updates', 'fedoracommunity.updates/table',
-                                       css_class='main_table',
-                                       params={'rows_per_page': 5,
-                                               'filters':{'status':'pending'}})
-    testing_updates_app = MokshaApp('Testing Updates', 'fedoracommunity.updates/table',
-                                       css_class='secondary_table',
-                                       params={'rows_per_page': 5,
-                                               'filters':{'status':'testing'}})
-    stable_updates_app = MokshaApp('Stable Updates', 'fedoracommunity.updates/table',
-                                       css_class='secondary_table',
-                                       params={'rows_per_page': 5,
-                                               'filters':{'status':'stable'}})
+    layout = [
+            Category('right-content-column', (
+                Widget('Filters', updates_filter),
+                MokshaApp('Alerts', 'fedoracommunity.alerts')
+                )),
+            Category('left-content-column', (
+                MokshaApp('Unpushed Updates', 'fedoracommunity.updates/table',
+                          css_class='main_table', params={
+                              'rows_per_page': 5,
+                              'filters': {
+                                  'status':'pending',
+                                  'profile': True
+                                  }
+                              }),
+                MokshaApp('Testing Updates', 'fedoracommunity.updates/table',
+                          css_class='secondary_table', params={
+                              'rows_per_page': 5,
+                              'filters': {
+                                  'status':'testing',
+                                  'profile': True
+                                  }
+                              }),
+                MokshaApp('Stable Updates', 'fedoracommunity.updates/table',
+                          css_class='secondary_table', params={
+                              'rows_per_page': 5,
+                              'filters': {
+                                  'status':'stable',
+                                  'profile': True
+                                  }
+                              }),
+                ))
+    ]
 
-    layout = [Category('right-content-column',
-                        (Widget('Filters', updates_filter
-                                 ),
-                        MokshaApp('Alerts', 'fedoracommunity.alerts'))),
-              Category('left-content-column',
-                        (unpushed_updates_app,
-                        testing_updates_app,
-                        stable_updates_app))]
 
-    def update_params(self, d):
-        super(UpdatesContainer, self).update_params(d)
-
+anonymous_updates_container = AnonymousUpdatesContainer('updates_container')
 updates_container = UpdatesContainer('updates_container')
 
 class RootController(Controller):
 
-    # do something for index, this should be the container stuff
     @expose('mako:moksha.templates.widget')
     def index(self, **kwds):
         options = {}
-
-        tmpl_context.widget = updates_container
+        if request.identity:
+            tmpl_context.widget = updates_container
+        else:
+            tmpl_context.widget = anonymous_updates_container
         return {'options':options}
 
     @expose('mako:moksha.templates.widget')
