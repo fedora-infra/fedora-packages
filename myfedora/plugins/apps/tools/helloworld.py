b5a82ce04fe51338326b25650a6c198a816c73c5
John (J5) Palmieri <johnp@redhat.com>
2009-04-13 16:29:31 -0400

    remove some of the last remenents of myfedora

--- myfedora/plugins/apps/tools/helloworld.py
+++
@@ -1,9 +0,0 @@
-from myfedora.widgets.resourceview import ToolWidget
-from myfedora.lib.app_factory import AppFactory
-
-class HelloWorldToolApp(AppFactory):
-    entry_name = "tools.helloworld"
-
-class HelloWorldToolWidget(ToolWidget):
-    template = 'genshi:myfedora.plugins.apps.tools.templates.hello'
-    display_name = "Hello World Test"
