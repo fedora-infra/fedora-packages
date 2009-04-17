875133d31d39e30d4f3e018e450ab37ec51f5dad
John (J5) Palmieri <johnp@redhat.com>
2009-04-17 12:54:27 -0400

    make work with alternate mounts

--- fedoracommunity/mokshaapps/packages/controllers/downloads.py
+++ fedoracommunity/mokshaapps/packages/controllers/downloads.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 class ReleaseDownloadsFilter(SingleSelectField):
     options = []
     attrs = {'onchange': """
-        moksha.html_load(moksha.csrf_rewrite_url('/apps/fedoracommunity.packages/package/downloads/downloads'), {'package': package_name, 'release': $('#downloads_widget_releases').val()}, function(r) {
+        moksha.html_load(moksha.url('/apps/fedoracommunity.packages/package/downloads/downloads'), {'package': package_name, 'release': $('#downloads_widget_releases').val()}, function(r) {
                     var $stripped = moksha.filter_resources(r);
                     $('div.package_downloads').html($stripped);
             }, $("#" + uid + " .overlay"));"""}
