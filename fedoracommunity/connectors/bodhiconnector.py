87598863a02e6e8e200098b72253bf55d3b56611
Luke Macken <lmacken@redhat.com>
2009-05-05 18:53:40 -0400

    Minor comment tweaks

--- fedoracommunity/connectors/bodhiconnector.py
+++ fedoracommunity/connectors/bodhiconnector.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -191,10 +191,11 @@ class BodhiConnector(IConnector, ICall, IQuery):
         params.update(filters)
         params['tg_paginate_no'] = int(start_row/rows_per_page) + 1
 
-        # Ask for twice as many updates.  This is so we can handle the case
-        # where there are two updates for each package, one for each release.
-        # Yes, worst case we get twice as much data as we ask for, but this
-        # allows us to do *much* more efficient database calls on the server.
+        # If we're grouping updates, ask for twice as much.  This is so we can
+        # handle the case where there are two updates for each package, one for
+        # each release.  Yes, worst case we get twice as much data as we ask
+        # for, but this allows us to do *much* more efficient database calls on
+        # the server.
         if group_updates:
             params['tg_paginate_limit'] = rows_per_page * 2
         else:
