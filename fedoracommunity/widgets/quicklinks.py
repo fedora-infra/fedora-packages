1540fd2c171e90e68273e5fd18eb60bd79172767
John (J5) Palmieri <johnp@redhat.com>
2009-05-07 23:32:44 -0400

    add links to search

--- fedoracommunity/widgets/quicklinks.py
+++ fedoracommunity/widgets/quicklinks.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -32,7 +32,15 @@ class QuickLinksWidget(Selectable):
              ('Package Maintenance',
               'View my pending updates',
               '/package_maintenance/updates/my_unpushed',
-              'view_my_pending_updates')]
+              'view_my_pending_updates'),
+             ('Search',
+              'Search for packages',
+              '/search/?st=packages',
+              'search_packages'),
+              ('Search',
+              'Search for people',
+              '/search/?st=people',
+              'search_people')]
     @staticmethod
 
     def add_link(d, category, label, link, content_id):
