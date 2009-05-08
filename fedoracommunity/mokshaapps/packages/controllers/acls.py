6616789b887431e29a6ff5b1dfa08cdb9204b9d2
John (J5) Palmieri <johnp@redhat.com>
2009-05-08 10:45:08 -0400

    seperate users from groups and display all for ACL's

--- fedoracommunity/mokshaapps/packages/controllers/acls.py
+++ fedoracommunity/mokshaapps/packages/controllers/acls.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -24,23 +24,30 @@ from helpers import PackagesDashboardContainer
 
 class AclsGrid(Grid, ContextAwareWidget):
     template='mako:fedoracommunity.mokshaapps.packages.templates.acls_table_widget'
-
-    def update_params(self, d):
-        d['resource'] = 'pkgdb',
-        d['resource_path'] = 'acls'
-        super(AclsGrid, self).update_params(d)
+    resource = 'pkgdb'
+    resource_path ='acls'
 
 acls_grid = AclsGrid('acls_grid')
 
 class AclsDashboard(PackagesDashboardContainer):
     template = 'mako:fedoracommunity.mokshaapps.packages.templates.single_col_dashboard'
-    layout = [Category('content-col-apps',Widget('Users', acls_grid,
-                                                 params={'rows_per_page': 10,
+    layout = [Category('content-col-apps',(Widget('Users', acls_grid,
+                                                 params={'rows_per_page': 999999,
                                                          'filters': {'package': '',
-                                                                    'roles': ['owner', 'maintainer', 'watcher']
+                                                                    'roles': ['owner', 'maintainer', 'watcher'],
+                                                                    'type': 'users'
+                                                                    }
+                                                        }
+                                                 ),
+                                             Widget('Groups', acls_grid,
+                                                 params={'rows_per_page': 999999,
+                                                         'filters': {'package': '',
+                                                                    'roles': ['owner', 'maintainer', 'watcher'],
+                                                                    'type': 'groups'
                                                                     }
                                                         }
                                                  )
+                                            )
                       )
              ]
 
@@ -51,6 +58,7 @@ class AclsController(Controller):
     def index(self, package, roles=None):
         if not roles:
             roles = ['owner', 'maintainer', 'watcher']
+
         tmpl_context.widget = acls_dashboard
         return {'options':{'package': package, 'roles': roles}}
 
