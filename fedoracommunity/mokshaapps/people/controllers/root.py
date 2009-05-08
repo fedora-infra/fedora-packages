2f6d3edb043c9e607b268883b538d0701d0c7d22
John (J5) Palmieri <johnp@redhat.com>
2009-05-08 19:18:17 -0400

    found more quicklink->task renames and fixed css class in peoples > profile

--- fedoracommunity/mokshaapps/people/controllers/root.py
+++ fedoracommunity/mokshaapps/people/controllers/root.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -50,8 +50,8 @@ class ProfileContainer(DashboardContainer, ContextAwareWidget):
               Category('right-content-column-apps',
                        (MokshaApp('Your Packages', 'fedoracommunity.packages/mypackages'),
                         MokshaApp('Alerts', 'fedoracommunity.alerts'),
-                        MokshaWidget('Quick Links', 'fedoracommunity.quicklinks', auth=not_anonymous())),
-                        default_child_css="panel",
+                        MokshaWidget('Tasks', 'fedoracommunity.quicklinks', auth=not_anonymous())),
+                        default_child_css='app panel',
                       ),
               Category('left-content-column-apps',
                        (MokshaApp('Your Group Memberships',
@@ -79,7 +79,8 @@ class PeopleContainer(DashboardContainer, ContextAwareWidget):
                         (MokshaApp('Packages', 'fedoracommunity.packages/userpackages',
                                   params={'username':''}),
                          MokshaApp('Alerts', 'fedoracommunity.alerts'),
-                         MokshaWidget('Quick Links', 'fedoracommunity.quicklinks', auth=not_anonymous()))
+                         MokshaWidget('Tasks', 'fedoracommunity.quicklinks', auth=not_anonymous())),
+                         default_child_css='app panel'
                         ),
               Category('left-content-column-apps',
                        (MokshaApp('Group Memberships', 'fedoracommunity.people/memberships/table',
