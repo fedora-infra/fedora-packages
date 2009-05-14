483701a8621e63fe24528a8c6251fb915150377f
Luke Macken <lmacken@redhat.com>
2009-05-14 17:28:38 -0400

    fedoracommunity.lib.helpers.parse_build fixes/tweaks

--- fedoracommunity/lib/helpers.py
+++ fedoracommunity/lib/helpers.py
@@ -1,19 +1,17 @@
 def parse_build(build):
     """
-    >>> nvr = parse_build('python-sqlalchemy-0.4.8-1.fc10.noarch')
+    >>> nvr = parse_build('python-sqlalchemy-0.4.8-1.fc10')
     >>> nvr['name']
     'python-sqlalchemy'
     >>> nvr['version']
     '0.4.8'
     >>> nvr['release']
     '1.fc10'
-    >>> nvr['arch']
-    'noarch'
     """
     chunks = build.split('-')
     return {
             'name': '-'.join(chunks[:-2]),
             'version': '-'.join(chunks[-2:-1]),
-            'release': '.'.join(chunks[-1].split('.')[:-1]),
-            'arch': chunks[-1].split('.')[-1],
-            }
+            'release': chunks[-1],
+            'nvr': build,
+           }
