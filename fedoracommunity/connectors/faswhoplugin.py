383350e68b67099029d4b1243f6066b64d59c40b
John (J5) Palmieri <johnp@redhat.com>
2009-05-19 17:29:55 -0400

    quote came_from login query string so it doesn't get mistaken as part of the url

--- fedoracommunity/connectors/faswhoplugin.py
+++ fedoracommunity/connectors/faswhoplugin.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2009  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -31,6 +31,7 @@ from repoze.who.classifiers import default_challenge_decider
 from repoze.who.interfaces import IChallenger, IIdentifier
 from Cookie import SimpleCookie
 from paste.request import parse_dict_querystring, parse_formvars
+from urllib import quote_plus
 
 from moksha.middleware.csrf import CSRFMetadataProvider
 from moksha.api.errorcodes import login_err
@@ -250,7 +251,7 @@ class FASWhoPlugin(object):
             environ['FAS_AUTH_ERROR'] = err
 
             err_app = HTTPFound(err_goto + '?' +
-                                'came_from=' + came_from +
+                                'came_from=' + quote_plus(came_from) +
                                 '&ec=' + login_err.USERNAME_PASSWORD_ERROR.code)
 
             environ['repoze.who.application'] = err_app
