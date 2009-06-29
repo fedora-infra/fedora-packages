067223fb53e230e5f68d54fa9ae4e5faf13a55bc
Ian Weller <ianweller@deathray.ianweller.org>
2009-06-29 16:33:09 -0500

    Attempt to make 'most active pages' a Grid and fail :)

---
+++ fedoracommunity/mokshaapps/statistics/templates/wiki_active_pages.mak
@@ -0,0 +1,32 @@
+<div class="list header-list">
+    <script type="text/javascript">
+        function _text_filter(text) {
+            var results = $("<div />");
+            var ul = $("<ul />");
+            results.append(ul);
+            var v=text.split('\n');
+            for (i in v) {
+                line = v[i];
+                ul.append("<li>" + line + "</li>");
+            }
+
+            return results.html();
+        }
+    </script>
+    <table id="${id}">
+        <thead>
+          <th>Page Name</th>
+          <th>Last edited by</th>
+        </thead>
+        <tbody class="rowtemplate">
+                <tr>
+                    <td>
+                        <a href="https://fedoraproject.org/wiki/@{title}" target="_blank">@{title}</a>
+                    </td>
+                    <td>
+                        @{last_edited_by}
+                    </td>
+                </tr>
+            </tbody>
+    </table>
+</div>
--- fedoracommunity/mokshaapps/statistics/widgets/wiki.py
+++ fedoracommunity/mokshaapps/statistics/widgets/wiki.py
@@ -17,19 +17,23 @@
 from pylons import cache
 from fedora.client import Wiki
 from datetime import datetime, timedelta
+from moksha.api.widgets import Grid
 from moksha.api.widgets.containers import DashboardContainer
 from moksha.lib.helpers import Category, Widget as MokshaWidget, defaultdict
 from fedoracommunity.widgets.flot import FlotWidget
 
-def get_wiki_statistics(days=7, show=5):
-    """ Get some wiki statistics in flot format """
+def get_wiki_statistics(days=7, show=10):
+    """ Get some wiki statistics in multiple formats """
     stats = {
-        'most_active_pages': {'data': [], 'options': {'xaxis': {}}},
+        # most_active_pages is a Grid
+        'most_active_pages': [],
+        # most_active_users is a FlotWidget
         'most_active_users': {'data': [], 'options': {'xaxis': {}}},
     }
 
     users = defaultdict(list) # {username: [change,]}
     pages = defaultdict(int)  # {pagename: # of edits}
+    pages_last_edited_by = {} # {pagename: username}
 
     # FIXME: I don't think the Wiki object pays attention to `now`...
     now = datetime.utcnow()
@@ -41,6 +45,8 @@ def get_wiki_statistics(days=7, show=5):
     for change in changes:
         users[change['user']].append(change['title'])
         pages[change['title']] += 1
+        if change['title'] not in pages_last_edited_by.keys():
+            pages_last_edited_by[change['title']] = change['user']
 
     ## Determine the most active wiki pages
     most_active_pages = sorted(pages.items(),
@@ -48,16 +54,11 @@ def get_wiki_statistics(days=7, show=5):
             reverse=True)[:show]
 
     page_data = []
-    page_ticks = []
     for i, page in enumerate(most_active_pages):
-        page_data.append([i, page[1]])
-        page_ticks.append([i + 0.5, page[0]])
+        page_data.append({'title': page, 'last_edited_by':
+                          pages_last_edited_by[page[0]]})
 
-    stats['most_active_pages']['options']['xaxis']['ticks'] = page_ticks
-    stats['most_active_pages']['data'].append({
-        'data': page_data,
-        'bars': {'show': True}
-        })
+    stats['most_active_pages'] = page_data
 
     ## Determine the most active wiki users
     most_active_users = sorted(users.items(),
@@ -80,7 +81,8 @@ def get_wiki_statistics(days=7, show=5):
     return stats
 
 
-class MostActiveWikiPages(FlotWidget):
+class MostActiveWikiPages(Grid):
+    template = 'mako:fedoracommunity.mokshaapps.statistics.templates.wiki_active_pages'
 
     # TODO: Make these more configurable.  Ideally we would want them to be
     # configurable in the Widget.params, but those don't get populated properly
@@ -94,8 +96,7 @@ class MostActiveWikiPages(FlotWidget):
         stats = wiki_cache.get_value(key='metrics',
                 createfunc=get_wiki_statistics,
                 expiretime=3600)
-        d.data = stats['most_active_pages']['data']
-        d.options = stats['most_active_pages']['options']
+        d.data = stats['most_active_pages']
         super(MostActiveWikiPages, self).update_params(d)
 
 
