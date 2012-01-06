5b774b3c54fecbf6c1750d76762f47a94338a27b
Luke Macken <lmacken@redhat.com>
2012-01-06 18:09:07 -0500

    Disable some duplicate global resources that are getting pulled in

--- setup.py
+++ setup.py
@@ -122,10 +122,10 @@ setup(
     fedoracommunity_960_24_col_cs = fedoracommunity.widgets:fedoracommunity_960_24_col_css
     fedoracommunity_appchrome_css = fedoracommunity.widgets:fedoracommunity_appchrome_css
     fedoracommunity_branding_css = fedoracommunity.widgets:fedoracommunity_branding_css
-    jquery_json_js = moksha.widgets.json:jquery_json_js
+    #jquery_json_js = moksha.widgets.json:jquery_json_js
     #jquery_ui_tabs = tw.jquery.ui_tabs:jquery_ui_tabs_js
-    moksha_js = moksha.widgets.moksha_js:moksha_js
-    jquery_template_js = moksha.widgets.jquery_template:jquery_template_js
+    #moksha_js = moksha.widgets.moksha_js:moksha_js
+    #jquery_template_js = moksha.widgets.jquery_template:jquery_template_js
 
     [moksha.connector]
     koji = fedoracommunity.connectors:KojiConnector
