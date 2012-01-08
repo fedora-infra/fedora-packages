53f9a146ed34e741cf1401b7c78cc7dfd1274f2c
John (J5) Palmieri <johnp@redhat.com>
2012-01-08 17:07:32 -0500

    source repo doesn't have an arch

--- fedoracommunity/search/index.py
+++ fedoracommunity/search/index.py
@@ -116,7 +116,7 @@ class Indexer(object):
 
         yb.doConfigSetup(self.yum_conf, root=os.getcwd(), init_plugins=False)
         for r in yb.repos.findRepos('*'):
-            if r.id in ['rawhide-x86_64', 'rawhide-source-x86_64']:
+            if r.id in ['rawhide-x86_64', 'rawhide-source']:
                 r.enable()
             else:
                 r.disable()
