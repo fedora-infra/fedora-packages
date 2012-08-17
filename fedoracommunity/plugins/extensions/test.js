614131d2c61bc120876e36ed496505594d0e8fd5
Ralph Bean <rbean@redhat.com>
2012-08-17 14:38:48 -0400

    Match up braces to cut down on log spam.

--- fedoracommunity/plugins/extensions/test.js
+++ fedoracommunity/plugins/extensions/test.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-        {{  
+        {
            info: {consumes:['build_message'],
                  author: 'John (J5) Palmieri <johnp@redhat.com>',
                  version: '0.1',
