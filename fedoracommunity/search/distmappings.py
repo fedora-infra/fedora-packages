d5e9113fbbf03fa5fadb7014d0460c02052ecbf8
Ralph Bean <rbean@redhat.com>
2013-08-22 10:42:24 -0400

    Correct the woefully incorrect distmappings table.

--- fedoracommunity/search/distmappings.py
+++ fedoracommunity/search/distmappings.py
@@ -1,16 +1,21 @@
 # Global list of koji tags we care about
 tags = ({'name': 'Rawhide', 'tag': 'f20'},
-        {'name': 'Fedora 19', 'tag': 'f16-updates'},
-        {'name': 'Fedora 19', 'tag': 'f16'},
-        {'name': 'Fedora 19 Testing', 'tag': 'f16-updates-testing'},
-        {'name': 'Fedora 18', 'tag': 'f16-updates'},
-        {'name': 'Fedora 18', 'tag': 'f16'},
-        {'name': 'Fedora 18 Testing', 'tag': 'f16-updates-testing'},
-        {'name': 'Fedora 17', 'tag': 'f16-updates'},
-        {'name': 'Fedora 17', 'tag': 'f16'},
-        {'name': 'Fedora 17 Testing', 'tag': 'f16-updates-testing'},
+
+        {'name': 'Fedora 19', 'tag': 'f19-updates'},
+        {'name': 'Fedora 19', 'tag': 'f19'},
+        {'name': 'Fedora 19 Testing', 'tag': 'f19-updates-testing'},
+
+        {'name': 'Fedora 18', 'tag': 'f18-updates'},
+        {'name': 'Fedora 18', 'tag': 'f18'},
+        {'name': 'Fedora 18 Testing', 'tag': 'f18-updates-testing'},
+
+        {'name': 'Fedora 17', 'tag': 'f17-updates'},
+        {'name': 'Fedora 17', 'tag': 'f17'},
+        {'name': 'Fedora 17 Testing', 'tag': 'f17-updates-testing'},
+
         {'name': 'EPEL 6', 'tag': 'dist-6E-epel'},
         {'name': 'EPEL 6', 'tag': 'dist-6E-epel-testing'},
+
         {'name': 'EPEL 5', 'tag': 'dist-5E-epel'},
         {'name': 'EPEL 5', 'tag': 'dist-5E-epel-testing'},
        )
