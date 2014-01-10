fedora-infra/fedora-packages

commit 62e65f7d0780eac2263cd72e60924abcd3ebc089

PEP8: config package

fedoracommunity/config/app_cfg.py
@@ -14,12 +14,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from fedoracommunity.lib import app_globals
 import fedoracommunity
 import fedoracommunity.lib
 
 from tg.configuration import AppConfig
-from bunch import Bunch
 from paste.deploy.converters import asbool
 try:
     # TG-2.3
@@ -27,6 +25,7 @@ try:
 except ImportError:
     # Earlier TG2
     from pylons.i18n import ugettext
+
 
 class FedoraCommunityConfig(AppConfig):
     tw2_initialized = False
@@ -36,11 +35,13 @@ class FedoraCommunityConfig(AppConfig):
             return app
 
         from tg import config
-        from tw2.core.middleware import Config, TwMiddleware
-        default_tw2_config = dict( default_engine=self.default_renderer,
-                                   translator=ugettext,
-                                   auto_reload_templates=asbool(self.get('templating.mako.reloadfromdisk', 'false'))
-                                   )
+        from tw2.core.middleware import TwMiddleware
+        default_tw2_config = dict(
+            default_engine=self.default_renderer,
+            translator=ugettext,
+            auto_reload_templates=asbool(
+                self.get('templating.mako.reloadfromdisk', 'false'))
+        )
         res_prefix = config.get('fedoracommunity.resource_path_prefix')
         if res_prefix:
             default_tw2_config['res_prefix'] = res_prefix
@@ -55,12 +56,13 @@ class FedoraCommunityConfig(AppConfig):
 
 #    def add_auth_middleware(self, app, *args):
 #        """ Add our FAS authentication middleware """
-#        from fedoracommunity.connectors.faswhoplugin import fas_make_who_middleware
+#        from fedoracommunity.connectors.faswhoplugin import \
+#            fas_make_who_middleware
 #        #from repoze.what.plugins.pylonshq import booleanize_predicates
 #        from copy import copy
 #        import logging
 #
-#        # TODO: go through moksha.lib.helpers and clean up the Predicate usage.
+#        # TODO: go through moksha.lib.helpers and clean up the Predicate usage
 #        # Eventually we want to be using this, because this is how TG2/Pylons
 #        # does it, however it currently breaks things for us...
 #        #booleanize_predicates()
@@ -93,7 +95,8 @@ base_config.auto_reload_templates = True
 base_config.use_legacy_renderer = False
 
 # Configure the base SQLALchemy Setup
-base_config.use_sqlalchemy = False # fix this later
+# fix this later
+base_config.use_sqlalchemy = False
 # base_config.model = myfedora.model
 # base_config.DBSession = myfedora.model.DBSession
 

fedoracommunity/config/environment.py
@@ -1,16 +1,16 @@
 # This file is part of Fedora Community.
 # Copyright (C) 2008-2010  Red Hat, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 

fedoracommunity/config/middleware.py
@@ -25,6 +25,7 @@ except ValueError:
 
 make_base_app = base_config.setup_tg_wsgi_app(load_environment)
 
+
 def make_app(global_conf, full_stack=True, **app_conf):
     from moksha.wsgi.middleware import make_moksha_middleware
     from fedoracommunity.connectors.api.mw import FCommConnectorMiddleware
@@ -45,20 +46,20 @@ def make_app(global_conf, full_stack=True, **app_conf):
     if base_config.squeeze:
         from repoze.squeeze.processor import ResourceSqueezingMiddleware
         app = ResourceSqueezingMiddleware(
-                app,
-                cache_dir='public/cache',
-                url_prefix='/cache/',
-                )
+            app,
+            cache_dir='public/cache',
+            url_prefix='/cache/',
+        )
 
     if base_config.profile:
         from repoze.profile.profiler import AccumulatingProfileMiddleware
         app = AccumulatingProfileMiddleware(
-                app,
-                log_filename='profile.log',
-                discard_first_request=True,
-                cachegrind_filename='./cachegrind.out',
-                flush_at_shutdown=True,
-                path='/__profile__'
-                )
+            app,
+            log_filename='profile.log',
+            discard_first_request=True,
+            cachegrind_filename='./cachegrind.out',
+            flush_at_shutdown=True,
+            path='/__profile__'
+        )
 
     return app

commit 67632cadd3c5b1d3c58d73a3ac564164c2ce6806

Fix regression introduced in 62e65f7d0780eac2263cd72e60924abcd3ebc089.

fedoracommunity/config/app_cfg.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from fedoracommunity.lib import app_globals
 import fedoracommunity
 import fedoracommunity.lib
 