56c5c1d7741edc5d8171cc9a93b49bf963c25b99
Ralph Bean <rbean@redhat.com>
2014-02-09 16:54:01 -0500

    2.0.13

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ packages = find_packages(exclude=['ez_setup'])
 
 setup(
     name='fedoracommunity',
-    version='2.0.12',
+    version='2.0.13',
     description='',
     license='AGPLv3',
     authors=('John (J5) Palmieri <johnp@redhat.com>',
